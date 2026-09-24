11fa6db33ad7a6c96e1b809adbf1eba3cd2a1289
MUHAMMAD FURQON <muhammadfurqon.dicoding@gmail.com>
2026-09-24 13:57:39 +0700

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// // https://astro.build/config
-// export default defineConfig({
-//   // GANTI dengan alamat website Anda setelah punya domain (dipakai untuk SEO).
-//   site: 'https://mangrove-mayangan.example.com',
-//   vite: {
-//     plugins: [tailwindcss()],
-//   },
-// });
 export default defineConfig({
-  // Ganti dengan URL GitHub Pages Anda
-  site: 'https://github.io', 
-  // HAPUS atau JANGAN gunakan baris 'base' di bawah jika nama repositori Anda adalah 'username.github.io'
-  // TAPI jika nama repositori Anda bebas (misal: 'my-portfolio'), isi seperti di bawah:
-  base: '/wisata-mayangan', 
+  site: 'https://furqonc4kcuk.github.io', // Ganti dengan username GitHub Anda
+  base: '/wisata-mayangan',           // WAJIB menggunakan garis miring '/' di awal
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
